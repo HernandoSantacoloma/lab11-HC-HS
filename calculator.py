f58b62b3ae817bfc7e85c24a5edabb5d3da57f8a
HernandoSantacoloma <hernando24@ufl.edu>
2026-04-08 13:50:01 -0400

    calculator

--- calculator.py
+++ calculator.py
@@ -34,7 +34,7 @@ def square_root(a):
 def subtract(a, b):
     return a - b
 
-def logarithm(a, b)
+def logarithm(a, b):
     if a <= 0 or b <= 0 or a == 1:
         raise ValueError("a and b must by greater than 0, and a cannot equal 1")
     return math.log(b) / math.log(a)
